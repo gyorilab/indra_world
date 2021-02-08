c05266d3ae6ac5cd51e0c22a391bcd22a36e0800
Ben Gyori <ben.gyori@gmail.com>
2021-02-08 00:10:35 -0500

    Update grounding filter test

--- indra_wm_service/tests/test_assembly.py
+++ indra_wm_service/tests/test_assembly.py
@@ -1,33 +1,44 @@
-from indra.statements import Event, Concept
-from indra.tools import assemble_corpus as ac
+from indra.statements import Concept
 from indra_wm_service.assembly.operations import *
 
 
 def test_compositional_grounding_filter():
+    # Test property filtered out based on score
     wm = [[('x', 0.5), ('y', 0.8), None, None]]
     concept = Concept('x', db_refs={'WM': wm})
     stmt = Event(concept)
     stmt_out = compositional_grounding_filter_stmt(stmt, 0.7, [])
+    concept = stmt_out.concept
     assert concept.db_refs['WM'][0][0] == ('y', 0.8), concept.db_refs
     assert concept.db_refs['WM'][0][1] is None
 
+    # Test property being promoted to theme
     wm = [[None, ('y', 0.8), None, None]]
     concept.db_refs['WM'] = wm
+    stmt = Event(concept)
     stmt_out = compositional_grounding_filter_stmt(stmt, 0.7, [])
+    concept = stmt_out.concept
     assert concept.db_refs['WM'][0][0] == ('y', 0.8), concept.db_refs
     assert concept.db_refs['WM'][0][1] is None
 
+    # Test score threshold being equal to score
     wm = [[('x', 0.7), ('y', 0.7), None, None]]
     concept.db_refs['WM'] = wm
+    stmt = Event(concept)
     stmt_out = compositional_grounding_filter_stmt(stmt, 0.7, [])
+    concept = stmt_out.concept
     assert concept.db_refs['WM'][0][0] == ('x', 0.7), concept.db_refs
     assert concept.db_refs['WM'][0][1] == ('y', 0.7), concept.db_refs
 
+    # Score filter combined with groundings to exclude plus promoting
+    # a property to a theme
     wm = [[('wm_compositional/entity/geo-location', 0.7), ('y', 0.7),
            None, None]]
     concept.db_refs['WM'] = wm
+    stmt = Event(concept)
     stmt_out = compositional_grounding_filter_stmt(stmt, 0.7,
         ['wm_compositional/entity/geo-location'])
+    concept = stmt_out.concept
     assert concept.db_refs['WM'][0][0] == ('y', 0.7), concept.db_refs
 
 
