6ec5f50415471124a3980f29045488cf4413e6da
Ben Gyori <ben.gyori@gmail.com>
2021-02-10 09:20:07 -0500

    Refactor reader processing

--- indra_wm_service/sources/dart.py
+++ indra_wm_service/sources/dart.py
@@ -21,6 +21,32 @@ def fix_provenance(stmts, doc_id):
     return stmts
 
 
+def process_reader_output(reader, reader_output_str, doc_id,
+                          grounding_mode, extract_filter):
+    if reader == 'eidos':
+        pr = eidos.process_json_str(reader_output_str,
+                                    grounding_mode=grounding_mode,
+                                    extract_filter=extract_filter)
+    elif reader == 'hume':
+        jld = json.loads(reader_output_str)
+        pr = hume.process_jsonld(jld, grounding_mode=grounding_mode,
+                                 extract_filter=extract_filter)
+    elif reader == 'sofia':
+        jd = json.loads(reader_output_str)
+        pr = sofia.process_json(jd, grounding_mode=grounding_mode,
+                                extract_filter=extract_filter)
+    elif reader == 'cwms':
+        pr = cwms.process_ekb(reader_output_str, grounding_mode=grounding_mode,
+                              extract_filter=extract_filter)
+    else:
+        raise ValueError('Unknown reader %s' % reader)
+    if pr is not None:
+        stmts = fix_provenance(pr.statements, doc_id)
+        return stmts
+    else:
+        return []
+
+
 def process_reader_outputs(outputs, corpus_id, grounding_mode='compositional',
                            extract_filter=None):
     if not extract_filter:
@@ -32,32 +58,15 @@ def process_reader_outputs(outputs, corpus_id, grounding_mode='compositional',
             with open(fname, 'rb') as fh:
                 all_stmts += pickle.load(fh)
                 continue
-
-        reader_stmts = []
         logger.info('Processing %d outputs for %s' %
                     (len(reader_outputs), reader))
-        for doc_id, reader_output_str in tqdm.tqdm(reader_outputs.items()):
-            if reader == 'eidos':
-                pr = eidos.process_json_str(reader_output_str,
-                                            grounding_mode=grounding_mode,
-                                            extract_filter=extract_filter)
-            elif reader == 'hume':
-                jld = json.loads(reader_output_str)
-                pr = hume.process_jsonld(jld, grounding_mode=grounding_mode,
-                                         extract_filter=extract_filter)
-            elif reader == 'sofia':
-                # FIXME: is this the right way to process Sofia output?
-                jd = json.loads(reader_output_str)
-                pr = sofia.process_json(jd, grounding_mode=grounding_mode,
-                                        extract_filter=extract_filter)
-            elif reader == 'cwms':
-                pr = cwms.process_ekb(reader_output_str,
-                                      grounding_mode=grounding_mode,
-                                      extract_filter=extract_filter)
-            else:
-                continue
-            if pr is not None:
-                reader_stmts += fix_provenance(pr.statements, doc_id)
+        reader_stmts = []
+        for doc_id, reader_output_str in reader_outputs.items():
+            reader_stmts += process_reader_output(reader,
+                                                  reader_output_str,
+                                                  doc_id,
+                                                  grounding_mode=grounding_mode,
+                                                  extract_filter=extract_filter)
         with open(fname, 'wb') as fh:
             pickle.dump(reader_stmts, fh)
         all_stmts += reader_stmts
