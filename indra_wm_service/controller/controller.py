c6235d5382965ca8e2d6ee998b1bfbcd0f0fbd50
Ben Gyori <ben.gyori@gmail.com>
2021-03-14 22:37:21 -0400

    Implement more controller functions

--- indra_wm_service/controller/controller.py
+++ indra_wm_service/controller/controller.py
@@ -1,6 +1,15 @@
+from indra.literature.dart_client import download_records
 from indra_wm_service.db.manager import DbManager
+from indra_wm_service.sources.dart import process_reader_output
 from indra_wm_service.assembly.incremental_assembler import \
     IncrementalAssembler
+from indra_wm_service.resources import get_resource_file
+from indra.pipeline import AssemblyPipeline
+from indra_wm_service.assembly.operations import *
+
+
+preparation_pipeline = AssemblyPipeline.from_json_file(
+    get_resource_file('statement_preparation.json'))
 
 
 class ServiceController:
@@ -31,3 +40,30 @@ class ServiceController:
 
     def remove_project(self, project_id):
         self.assemblers.pop(project_id, None)
+
+    def get_reader_output(self, record):
+        # TODO: should we unpack this here and return just the output?
+        return download_records([record])
+
+    def add_reader_output(self, content, reader, reader_version, document_id):
+        stmts = process_reader_output(reader, content, document_id, content)
+        prepared_stmts = preparation_pipeline.run(stmts)
+        self.db.add_statements_for_document(document_id,
+                                            reader_version=reader_version,
+                                            indra_version=1.0,
+                                            stmts=prepared_stmts)
+        # We need to check here if these statements need to be incrementally
+        # assembled into any projects. Do we do that every time or only when
+        # all the readings have become available?
+
+    def add_curation(self, project_id, curation):
+        self.db.add_curation_for_project(project_id, curation)
+
+    def add_project_documents(self, project_id, doc_ids):
+        self.db.add_documents_for_project(project_id, doc_ids)
+        # TODO: we need to check here if there are already prepared
+        # statements for these documents. If there are then we can
+        # run incremental assembly and return an assembly delta.
+
+    def add_dart_record(self, reader, reader_version, document_id, date):
+        self.db.add_dart_record(reader, reader_version, document_id, date)
