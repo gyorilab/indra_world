04befb2ec9aa1b23d46c239cbffa0a00e95c14b2
Ben Gyori <ben.gyori@gmail.com>
2021-03-15 12:49:50 -0400

    Fixes to incremental assembly

--- indra_wm_service/assembly/incremental_assembler.py
+++ indra_wm_service/assembly/incremental_assembler.py
@@ -1,9 +1,11 @@
 import networkx
 from collections import defaultdict
 from indra.belief import extend_refinements_graph, BeliefEngine
+from indra.ontology.world import world_ontology
 from indra.preassembler.refinement import RefinementConfirmationFilter
 from indra_wm_service.assembly.operations import CompositionalRefinementFilter
-from indra_wm_service.assembly.operations import location_matches_compositional
+from indra_wm_service.assembly.operations import \
+    location_matches_compositional, location_refinement_compositional
 
 
 class IncrementalAssembler:
@@ -13,17 +15,20 @@ class IncrementalAssembler:
         # These are preassembly data structures
         self.stmts_by_hash = {}
         self.evs_by_stmt_hash = {}
+        self.refinement_edges = set()
+        self.prepared_stmts = prepared_stmts
+
+        crf = CompositionalRefinementFilter(ontology=world_ontology)
+        rcf = RefinementConfirmationFilter(ontology=world_ontology,
+            refinement_fun=location_refinement_compositional)
+        self.refinement_filters = [crf, rcf]
+        self.deduplicate()
+        self.get_refinements()
         self.refinements_graph = \
             self.build_refinements_graph(self.stmts_by_hash,
                                          self.refinement_edges)
-        self.refinement_edges = set()
-        self.prepared_stmts = prepared_stmts
-        self.refinement_filters = [CompositionalRefinementFilter,
-                                   RefinementConfirmationFilter]
         self.belief_engine = \
             BeliefEngine(refinements_graph=self.refinements_graph)
-        self.deduplicate()
-        self.get_refinements()
 
     def deduplicate(self):
         for stmt in self.prepared_stmts:
@@ -32,6 +37,8 @@ class IncrementalAssembler:
             if stmt_hash not in self.stmts_by_hash:
                 stmt.evidence = []
                 self.stmts_by_hash[stmt_hash] = stmt
+            if stmt_hash not in self.evs_by_stmt_hash:
+                self.evs_by_stmt_hash[stmt_hash] = []
             self.evs_by_stmt_hash[stmt_hash] += evs
 
     def get_refinements(self):
@@ -40,8 +47,8 @@ class IncrementalAssembler:
         for stmt in self.prepared_stmts:
             refinements = None
             for filter in self.refinement_filters:
-                refs = filter.apply(stmt, refinements)
-                self.refinement_edges |= set(refs)
+                refinements = filter.get_related(stmt, refinements)
+                self.refinement_edges |= set(refinements)
 
     def build_refinements_graph(self, stmts_by_hash, refinement_edges):
         g = networkx.DiGraph()
@@ -98,8 +105,8 @@ class AssemblyDelta:
         return {
             'new_stmts': {sh: stmt.to_json()
                           for sh, stmt in self.new_stmts.items()},
-            'new_evidence': {sh: ev.to_json()
-                             for sh, ev in self.new_evidences.items()},
+            'new_evidence': {sh: [ev.to_json() for ev in evs]
+                             for sh, evs in self.new_evidences.items()},
             'new_refinements': list(self.new_refinements),
             'beliefs': self.beliefs
         }
