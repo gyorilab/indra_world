51e70108665353ee0819a1b5a6cc9a8d8faa251e
Ben Gyori <ben.gyori@gmail.com>
2021-05-04 22:58:58 -0400

    Remove some old imports

--- indra_world/__init__.py
+++ indra_world/__init__.py
@@ -3,12 +3,6 @@ from indra.config import get_config
 
 default_bucket = 'world-modelers'
 default_key_base = 'indra_models'
-file_defaults = {'raw': 'raw_statements',
-                 'sts': 'statements',
-                 'cur': 'curations',
-                 'meta': 'metadata'}
-
-
 default_profile = 'wm'
 cache_config = get_config('INDRA_WM_CACHE')
 if cache_config:
@@ -16,11 +10,3 @@ if cache_config:
     CACHE.mkdir(exist_ok=True)
 else:
     CACHE = None
-
-
-class InvalidCorpusError(Exception):
-    pass
-
-
-from .corpus import Corpus
-from .curator import LiveCurator
