3d15af22aa306e4cd82d812c7b67c62c35d15dc4
Ben Gyori <ben.gyori@gmail.com>
2022-03-03 01:30:17 -0500

    Update script

--- scripts/feb2022/initial.py
+++ scripts/feb2022/initial.py
@@ -14,32 +14,33 @@ logger = logging.getLogger(__name__)
 
 if __name__ == '__main__':
     dc = DartClient()
-    readers = ['eidos', 'hume']
-    recs = [
-        r for r in dc.get_reader_output_records(readers)
-        if r['output_version'] == ontology_version
-    ]
+    with open('initial_doc_ids.txt', 'r') as fh:
+        doc_ids = {l.strip() for l in fh.readlines()}
+    recs = dc.get_reader_output_records(['hume'],
+        timestamp={'before': '2022-03-01T22:00:00'})
+    recs += dc.get_reader_output_records(['eidos'],
+        timestamp={'after': '2022-03-02T22:00:00'})
+    recs = [r for r in recs if r['doc_id'] in doc_ids]
+    recs = [r for r in recs if r['ontology_version'] == ontology_version]
 
+    print(len(recs))
 
-    ont_json = dc.get_ontology(ontology_version)
-    ontology = \
-        WorldOntology(url=None,
-                      yml=yaml.load(ont_json['ontology'],
-                                    Loader=yaml.FullLoader))
+    ontology = dc.get_ontology_graph(ontology_version)
     ontology.initialize()
 
-    corpus_id = 'feb2022_initial_dsmte'
+    corpus_id = 'feb2022_initial_dsmte_v2'
     meta_data = {
         'corpus_id': corpus_id,
-        'description': 'February 2022 embed initial corpus for DSMT-E',
-        'display_name': 'Feb. 2022 Initial DSMT-E',
+        'description': 'February 2022 embed initial corpus for DSMT-E v2 with ' \
+            'grounding improvements',
+        'display_name': 'Feb. 2022 Initial DSMT-E v2',
         'readers': readers,
         'assembly': {
             'level': 'grounding_location',
             'grounding_threshold': 0.6,
         },
         'num_statements': 0,
-        'num_documents': 272
+        'num_documents': 275
     }
 
     logger.info('Using metadata: %s' % json.dumps(meta_data, indent=1))
