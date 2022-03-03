c2ae4b14b67f4139c6bc642975882a8b1fb2712e
Ben Gyori <ben.gyori@gmail.com>
2022-03-03 11:27:15 -0500

    Handle more corner cases for compositional grounding

--- indra_world/sources/eidos/processor.py
+++ indra_world/sources/eidos/processor.py
@@ -199,9 +199,16 @@ class EidosProcessorCompositional(EidosWorldProcessor):
                         compositional_entry[0] = compositional_entry[1]
                         compositional_entry[1] = None
                     # Promote theme process property into theme property
-                    if compositional_entry[3] and not compositional_entry[2] \
+                    if compositional_entry[3] and compositional_entry[0] and \
+                            not compositional_entry[2] \
                             and not compositional_entry[1]:
                         compositional_entry[1] = compositional_entry[3]
+                        compositional_entry[3] = None
+                    # If there is only a theme process property and nothing
+                    # else, we promote it to be the theme
+                    if compositional_entry[3] and not any(
+                            compositional_entry[:-1]):
+                        compositional_entry[0] = compositional_entry[3]
                         compositional_entry[3] = None
                     if any(compositional_entry):
                         entries.append(compositional_entry)
