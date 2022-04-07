c1f7ac5864843a299efdd45e52841c537925b7bc
Ben Gyori <ben.gyori@gmail.com>
2022-04-07 13:29:42 -0400

    Remove old dashboard files

--- indra_world/dashboard/__init__.py
+++
@@ -1,4 +0,0 @@
-"""The dashboard provides a simple web-based interface to run INDRA World
-assembly by searching for reader outputs under some constraints, specify
-assembly configurations, and parameters for the output (name, description,
-etc.)."""
